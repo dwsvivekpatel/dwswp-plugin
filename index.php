72ed696965bb2112fb14e9f5362eb343311c9ece
vivekdolphine <vivek.g@dolphinwebsolutions.com>
2023-11-22 13:24:37 +0530

    ARRAY commit

--- index.php
+++ index.php
@@ -27,3 +27,43 @@ echo "<br>";
 echo $mastang->get_name();
 echo "<br>";
 echo $ashish->get_name();
+
+
+
+
+class ArrayObjectExample
+{
+    private $data;
+
+    // Constructor to initialize the array
+    public function __construct()
+    {
+        $this->data = array();
+    }
+
+    // Method to add an element to the array
+    public function addElement($element)
+    {
+        $this->data[] = $element;
+    }
+
+    // Method to get the array
+    public function getArray()
+    {
+        return $this->data;
+    }
+}
+
+// Create an instance of the ArrayObjectExample class
+$arrayObject = new ArrayObjectExample();
+
+// Add elements to the array
+$arrayObject->addElement("Element 1");
+$arrayObject->addElement("Element 2");
+$arrayObject->addElement("Element 3");
+
+// Get the array
+$array = $arrayObject->getArray();
+
+// Print the array
+print_r($array);
